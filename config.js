e697d9a0d82624827f001981a44379981f483320
Cam Prier <CSPrier@gmail.com>
2018-05-01 14:24:00 -0700

    Created models/note.js, noteSchema and note model, and exported it

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 'use strict';
 
 exports.PORT = process.env.PORT || 8080;
+
+// Uniform Resource Identifier
+// This is used instead of DATABASE_URL because it syncs up with Heroku's mLab Add-On.
+// Can change to DATABASE_URL but you need to change the Config Vars in Heroku.
+exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful';
---
+++ models/note.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Models are where to define 'Schema'
+// Models are constructors which create documents which can be saved and retrieved from a database connection
+
+const mongoose = require('mongoose');
+
+// Note Schema and Model
+const noteSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  content: String,
+  createdAt: Date,
+  updatedAt: Date
+});
+
+// Create a model
+// 'Note' parameter is used to create a collection, it will be lowercase and pluralized after the collection is created => 'notes'
+const Note = mongoose.model('Note', noteSchema);
+
+// Export the model
+module.exports = {Note};
